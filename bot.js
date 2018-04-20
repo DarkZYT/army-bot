51921a791ff417d1a7a777a5705006678ce846aa
DarkZYT <aouabsss@gmail.com>
2018-04-20 22:07:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,10 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on('guildMemberAdd',b => {
+    b.guild.channels.find("type","text").send("Welcome <@" + b.id + "> to Army's discord server!")
+});
+
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
